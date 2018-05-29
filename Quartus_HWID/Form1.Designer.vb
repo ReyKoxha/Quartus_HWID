19b88bcc1c7435930a61cd7172e2130f3f0a0755
Rey Koxha <rey@koxha.de>
2018-05-29 02:08:22 +0200

    Init

---
+++ Quartus_HWID/Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lblNetwork = New System.Windows.Forms.Label()
+        Me.FormSkin1 = New Quartus_HWID.FormSkin()
+        Me.FlatTextBox1 = New Quartus_HWID.FlatTextBox()
+        Me.FlatMini1 = New Quartus_HWID.FlatMini()
+        Me.FlatClose1 = New Quartus_HWID.FlatClose()
+        Me.FormSkin1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblNetwork
+        '
+        Me.lblNetwork.AutoSize = True
+        Me.lblNetwork.Location = New System.Drawing.Point(12, 195)
+        Me.lblNetwork.Name = "lblNetwork"
+        Me.lblNetwork.Size = New System.Drawing.Size(0, 13)
+        Me.lblNetwork.TabIndex = 3
+        '
+        'FormSkin1
+        '
+        Me.FormSkin1.BackColor = System.Drawing.Color.White
+        Me.FormSkin1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(59, Byte), Integer), CType(CType(72, Byte), Integer))
+        Me.FormSkin1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(59, Byte), Integer), CType(CType(72, Byte), Integer))
+        Me.FormSkin1.Controls.Add(Me.FlatTextBox1)
+        Me.FormSkin1.Controls.Add(Me.FlatMini1)
+        Me.FormSkin1.Controls.Add(Me.FlatClose1)
+        Me.FormSkin1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FormSkin1.FlatColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FormSkin1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FormSkin1.HeaderColor = System.Drawing.Color.FromArgb(CType(CType(47, Byte), Integer), CType(CType(54, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.FormSkin1.HeaderMaximize = False
+        Me.FormSkin1.Location = New System.Drawing.Point(0, 0)
+        Me.FormSkin1.Name = "FormSkin1"
+        Me.FormSkin1.Size = New System.Drawing.Size(278, 107)
+        Me.FormSkin1.TabIndex = 5
+        Me.FormSkin1.Text = "Quartus HWID Generator"
+        '
+        'FlatTextBox1
+        '
+        Me.FlatTextBox1.BackColor = System.Drawing.Color.Transparent
+        Me.FlatTextBox1.Location = New System.Drawing.Point(13, 64)
+        Me.FlatTextBox1.MaxLength = 32767
+        Me.FlatTextBox1.Multiline = False
+        Me.FlatTextBox1.Name = "FlatTextBox1"
+        Me.FlatTextBox1.ReadOnly = False
+        Me.FlatTextBox1.Size = New System.Drawing.Size(254, 29)
+        Me.FlatTextBox1.TabIndex = 7
+        Me.FlatTextBox1.Text = "Generating HWID..."
+        Me.FlatTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.FlatTextBox1.TextColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.FlatTextBox1.UseSystemPasswordChar = False
+        '
+        'FlatMini1
+        '
+        Me.FlatMini1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FlatMini1.BackColor = System.Drawing.Color.White
+        Me.FlatMini1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FlatMini1.Font = New System.Drawing.Font("Marlett", 12.0!)
+        Me.FlatMini1.Location = New System.Drawing.Point(233, 13)
+        Me.FlatMini1.Name = "FlatMini1"
+        Me.FlatMini1.Size = New System.Drawing.Size(18, 18)
+        Me.FlatMini1.TabIndex = 6
+        Me.FlatMini1.Text = "FlatMini1"
+        Me.FlatMini1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'FlatClose1
+        '
+        Me.FlatClose1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FlatClose1.BackColor = System.Drawing.Color.White
+        Me.FlatClose1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(168, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.FlatClose1.Font = New System.Drawing.Font("Marlett", 10.0!)
+        Me.FlatClose1.Location = New System.Drawing.Point(257, 13)
+        Me.FlatClose1.Name = "FlatClose1"
+        Me.FlatClose1.Size = New System.Drawing.Size(18, 18)
+        Me.FlatClose1.TabIndex = 5
+        Me.FlatClose1.Text = "FlatClose1"
+        Me.FlatClose1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(278, 107)
+        Me.Controls.Add(Me.lblNetwork)
+        Me.Controls.Add(Me.FormSkin1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.RightToLeftLayout = True
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Quartus HWID"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.FormSkin1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblNetwork As Label
+    Friend WithEvents FormSkin1 As FormSkin
+    Friend WithEvents FlatTextBox1 As FlatTextBox
+    Friend WithEvents FlatMini1 As FlatMini
+    Friend WithEvents FlatClose1 As FlatClose
+End Class
